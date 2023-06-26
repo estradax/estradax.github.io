88b42f2447636a3574775607360fe23f7138334c
azharjk <azhr.project@gmail.com>
2023-06-26 15:57:17 +0700

    ci: add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://estradax.github.io',
+  base: '/portfolio',
   integrations: [tailwind(), solidJs()],
 });
