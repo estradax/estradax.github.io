38de924795090e3e1a569a63777b05f8e5ff3c61
azharjk <azhr.project@gmail.com>
2023-06-27 19:46:20 +0700

    ci: change base url for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://estradax.github.io',
-  base: '/portfolio',
+  site: "https://estradax.github.io",
   integrations: [tailwind(), solidJs()],
 });
