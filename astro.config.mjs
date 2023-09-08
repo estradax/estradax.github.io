b1bca9a8b4bc748a6912e1a60e0706894c8ddbfd
estradax <azharumuhammad@gmail.com>
2023-09-08 18:42:11 +0700

    change the look of main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://estradax.github.io",
-  integrations: [tailwind(), solidJs()],
+  integrations: [solidJs()],
 });
