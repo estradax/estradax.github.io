2aa6e14269e76a69bc1acfb768c2f2236e633bb3
estradax <azharumuhammad@gmail.com>
2023-09-08 19:47:15 +0700

    fix loader

---
+++ src/js/site.js
@@ -0,0 +1,82 @@
+"use strict";
+
+
+// this js file is shit just because the template
+
+import "./jquery.1.8.3.min.js";
+// import "./wow.min.js"
+import "./featherlight.min.js"
+import "./featherlight.gallery.min.js"
+import "./jquery.enllax.min.js"
+import "./jquery.scrollUp.min.js"
+import "./jquery.easing.min.js"
+import "./jquery.stickyNavbar.min.js"
+import "./jquery.waypoints.min.js"
+// import "./images-loaded.min.js"
+import "./lightbox.min.js"
+
+$(document).ready(function () {
+	/* Video Lightbox */
+	if (!!$.prototype.simpleLightboxVideo) {
+		$('.video').simpleLightboxVideo();
+	}
+
+	/*ScrollUp*/
+	if (!!$.prototype.scrollUp) {
+		$.scrollUp();
+	}
+
+	/*Responsive Navigation*/
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-trigger span").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$(this).removeClass("open");
+		} else {
+			$("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+			$(this).addClass("open");
+		}
+	});
+
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-mobile ul a").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$("#nav-trigger span").removeClass("open");
+		}
+	});
+
+	/* Sticky Navigation */
+	if (!!$.prototype.stickyNavbar) {
+		$('#header').stickyNavbar();
+	}
+
+	$('#content').waypoint(function (direction) {
+		if (direction === 'down') {
+			$('#header').addClass('nav-solid fadeInDown');
+		}
+		else {
+			$('#header').removeClass('nav-solid fadeInDown');
+		}
+	});
+
+});
+
+
+/* Preloader and animations */
+$(window).load(function () { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow-y': 'visible'});
+
+	/* WOW Elements */
+	if (typeof WOW == 'function') {
+		new WOW().init();
+	}
+
+	/* Parallax Effects */
+	if (!!$.prototype.enllax) {
+		$(window).enllax();
+	}
+
+});
